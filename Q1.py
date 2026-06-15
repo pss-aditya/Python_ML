702cdeb2ea97733b4eeb8719fcb28989c8f8d5c5
pss-aditya <adityavalekar2277@gmail.com>
2026-06-15 08:53:44 +0530

    Assignment 2 solutions

---
+++ Q1.py
@@ -0,0 +1,5 @@
+a = 10
+
+print("Value of a is:",a)
+print(type(a))
+print(id(a))
---
+++ Q6.py
@@ -0,0 +1,15 @@
+print("Enter the first number:")
+a = input()
+
+print("Enter the second Number :")
+b = input ()
+
+Add = int(a) + int(b)
+Multiplication = int(a) * int(b)
+Division = float(a) / float (b)
+Subtraction = int (a) - int(b)
+
+print(" Addition  is :", Add)
+print(" Multiplication  is :", Multiplication)
+print(" Division  is :", Division)
+print(" Subtraction  is :", Subtraction)
