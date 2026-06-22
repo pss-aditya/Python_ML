d5b74b75e840a7475ea3f3cc48d95d004f2bf930
pss-aditya <adityavalekar2277@gmail.com>
2026-06-22 14:40:35 +0530

    Removed duplicate root files

--- Q1.py
+++
@@ -1,5 +0,0 @@
-a = 10
-
-print("Value of a is:",a)
-print(type(a))
-print(id(a))
--- Q9.py
+++
@@ -1,5 +0,0 @@
-
-name = input("Enter the name: ")
-age = int(input("Enter your age : "))
-
-print("Hello ", name + ", you will turn", age + 1, "by next year .")
