20a32649559a365dab274d5e5c5d2b5cbc19e0cd
pss-aditya <adityavalekar2277@gmail.com>
2026-08-22 23:36:06 +0530

    Added Wine Prediction Case Study

---
+++ Machine_Learning/WinePrediction_CaseStudy/WinePredictor_KNN2.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+
+
+def MarvellousClassifier(DataPath):
+    Border = "*"*60
+    ##############################################
+    # Step 1 : Load the Dataset from CSV File
+    ##############################################
+    print(Border)
+    print("Step 1 : Load the Dataset from CSV File")
+    print(Border)
+    
+    df = pd.read_csv(DataPath)
+    
+    print(Border)
+    print("Some Entries from Dataset :")
+    print(df.head())
+    print(Border)
+    
+    ############################################
+    # Step 2 : Clean the Dataset
+    ############################################
+    print(Border)
+    print("Step 2 : Clean the Dataset")
+    print(Border)
+    
+    df.dropna(inplace = True)
+    
+    print("Shape of Dataset : ", df.shape)
+    print("Total Records    : ", df.shape[0]) # row
+    print("Total Columns    : ", df.shape[1]) # column
+    print(Border)
+
+def main():
+    MarvellousClassifier("WinePredictor.csv")
+
+
+if __name__ == "__main__":
+    main()
